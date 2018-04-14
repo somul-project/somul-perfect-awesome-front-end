959497f076adcf1d2ce131a6103140e9d41fbc5d
clucle <wjdenwls123@gmail.com>
2018-04-15 02:31:14 +0900

    feat: add validate function in default js

--- public/js/default.js
+++ public/js/default.js
@@ -3,7 +3,7 @@ toggle between hiding and showing the dropdown content */
 function clickDropBtn(id, dropList) {
     
     var dropbtn = document.getElementById(dropList).classList;
-
+    
     dropbtn.toggle("show");
     
     var child_image = document.querySelector('#' + id + ' .dropbtn-image');
@@ -40,7 +40,7 @@ function setDropBtnName(id, name) {
     var child = document.querySelector('#' + id + ' .dropbtn');
     child.innerHTML = name;
     child.style.color = "black";
-
+    
     var dropDownClicked = document.getElementById(id + "_isClicked");
     dropDownClicked.value = "true";
 }
@@ -61,25 +61,38 @@ ignoreOnClick(dropBtnIgnore);
 ignoreOnClick(dropBtnImageIgnore);
 
 // Validate
-function validateEmail(text) {
-    function validateEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
-function validatePhone(text) {
-
+function validatePhone(phone) {
+    var re = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+    return re.test(phone);
 }
 
 function validateNotEmpty(text) {
-
+    var trimed = text.trim();
+    return (trimed.length == 0? false: true);
 }
 
-function validatePassword(text) {
-
-}
-
-function validateDropDown() {
-//
+function validateDropDownChecked(dropDownList) {
+    
+    var i;
+    for (i = 0; i < dropDownList.length; i++) {
+        var id = dropDownList[i];
+        var dropDownClicked = document.getElementById(id + "_isClicked");
+        if (dropDownClicked.value !== "true") {
+            window.location.hash = '';
+            window.location.hash = '#' + id;
+            window.scrollBy(0, -30);
+            
+            var child = document.querySelector('#' + id + ' .dropbtn');
+            child.style.color = "red";
+            return false;
+        }
+    }
+    
+    return true;
 }
