ddf69863ef966c887b5150494b53485909dc8d82
Gihyeon Yang <dexterastin@gmail.com>
2018-04-15 00:00:36 +0900

    feat: successApplication 폴더 변경

---
+++ public/js/successApplication.js
@@ -0,0 +1,15 @@
+createWrapper([
+    createTitleText("신청이<br>완료되었습니다.")
+]);
+
+createWrapper([
+    createHr()
+]);
+
+createWrapper([
+    createPtag("24시간 안에 이메일 인증을 하고,<br>추가 정보를 입력해주세요.")
+]);
+
+createWrapper([
+    createButton("yellow", "추가정보 입력하기", "alert('추가정보')")
+]);
