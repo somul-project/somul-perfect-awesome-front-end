2d95bd34fbc4cc193f022ae2498c7c54f8b0a257
clucle <wjdenwls123@gmail.com>
2018-04-14 22:14:35 +0900

    feat: add definition js template

---
+++ public/js/application.js
@@ -0,0 +1,48 @@
+
+require("./helper.js");
+
+createPaper();
+
+createWrapper([
+    createTitleText("title")
+]);
+
+createWrapper([
+    createRadio(["강연자", "봉사자"])
+]);
+
+createWrapper([
+    createPlaneText("이름"),
+    createInput("text", "이름을 입력해 주세요.")
+]);
+
+createWrapper([
+    createPlaneText("이메일"),
+    createInput("email", "이메일을 입력해 주세요."),
+    createButton("blue", "인증번호 보내기")
+]);
+
+createWrapper([
+    createPlaneText("인증번호"),
+    createInput("text", "인증번호를 입력해주세요")
+]);
+
+createWrapper([
+    createPlaneText("휴대폰 번호"),
+    createInput("text", "휴대폰 번호를 입력해 주세요.")
+]);
+
+createWrapper([
+    createPlaneText("비밀번호 설정"),
+    createInput("text", "휴대폰 번호를 입력해 주세요."),
+    createInput("text", "재입력 해주세요")
+]);
+
+createWrapper([
+    createPlaneText("개인정보 수집"),
+    createCheckBox("동의"),
+]);
+
+createWrapper([
+    createButton("yellow", "완료")
+]);
---
+++ public/js/helper.js
@@ -0,0 +1,37 @@
+
+function createPaper() {
+
+}
+
+function createWrapper(list) {
+// wrap list
+}
+
+function createTitleText(text) {
+// title, <hr>
+}
+
+function createPlaneText(text) {
+
+}
+
+function createRadio(list) {
+
+}
+
+function createCheckBox(text) {
+
+}
+
+function createButton(type, text, f) {
+// type (yellow, blue)
+}
+
+// text, password, email
+function createInput(type, planeHolder) {
+
+}
+
+function createDropDown() {
+
+}
