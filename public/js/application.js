17dc0fc06d9319e2afa6ba5e485947e785d8cdd4
clucle <wjdenwls123@gmail.com>
2018-04-15 04:32:47 +0900

    feat: application page validate

--- public/js/application.js
+++ public/js/application.js
@@ -17,29 +17,29 @@ createWrapper([
 
 createWrapper([
     createPlaneText("이름"),
-    createInput("text", "이름을 입력해 주세요.")
+    createInput("text", null, "이름을 입력해 주세요.")
 ]);
 
 createWrapper([
     createPlaneText("이메일"),
-    createInput("email", "이메일을 입력해 주세요."),
-    createButton("blue", "인증번호 보내기")
+    createInput("email", "email", "이메일을 입력해 주세요."),
+    createButton("blue", "인증번호 보내기", "sendConfirmMail('email')")
 ]);
 
 createWrapper([
     createPlaneText("인증번호"),
-    createInput("text", "인증번호를 입력해주세요")
+    createInput("text", null, "인증번호를 입력해주세요")
 ]);
 
 createWrapper([
     createPlaneText("휴대폰 번호"),
-    createInput("text", "휴대폰 번호를 입력해 주세요.")
+    createInput("text", "phone", "휴대폰 번호를 입력해 주세요.")
 ]);
 
 createWrapper([
     createPlaneText("비밀번호 설정"),
-    createInput("text", "비밀번호를 입력해 주세요."),
-    createInput("text", "재입력 해주세요")
+    createInput("password", "password", "비밀번호를 입력해 주세요."),
+    createInput("password", "check_password","재입력 해주세요")
 ]);
 
 createWrapper([
@@ -48,5 +48,28 @@ createWrapper([
 ]);
 
 createWrapper([
-    createButton("yellow", "완료")
+    createButton("yellow", "완료", "submit()")
 ]);
+
+// Functions
+
+function sendConfirmMail(id) {
+    // validate
+    var email = document.getElementById(id);
+    if(!validateEmail(email.value)) {
+        alert("잘못 된 이메일 형식입니다.");
+        return false;
+    }
+    // send
+    // 살려줘요 TODO AM 02:46
+}
+
+function submit() {
+    /*
+    if(!validateRadio()) return false;
+    if(!validateNotEmpty()) return false;
+    if(!validatePhone()) return false;
+    if(!validatePasswordCheck()) return false;*/
+    if(!validateCheckBox()) return false;
+    // send
+}
